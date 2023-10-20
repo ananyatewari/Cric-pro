f4797b9c0f4b756d72f5f2eb3631f1e2aca85d8d
ananyatewari <ananyatewari0205@gmail.com>
2023-10-20 14:20:03 +0530

    done

---
+++ script.js
@@ -0,0 +1,96 @@
+const sehwagPic = document.getElementById("sehwag");
+const sachinPic = document.getElementById("sachin")
+const gautamPic = document.getElementById("gautam");
+const viratPic = document.getElementById("virat");
+const dhoniPic = document.getElementById("dhoni");
+const yuvrajPic = document.getElementById("yuvraj");
+const rainaPic = document.getElementById("raina");
+const harPic = document.getElementById("har");
+const khanPic = document.getElementById("khan");
+const munafPic = document.getElementById("munaf");
+const nathPic = document.getElementById("nath");
+const close = document.getElementById("close");
+const blur = document.getElementById("blur");
+// const table = document.getElementsByTagName("table");
+
+function closing(){
+    close.style.display = "none";
+    sehwagPic.style.display = "none";
+    sachinPic.style.display = "none";
+    gautamPic.style.display = "none";
+    viratPic.style.display = "none";
+    dhoniPic.style.display = "none";
+    yuvrajPic.style.display = "none";
+    rainaPic.style.display = "none";
+    harPic.style.display = "none";
+    khanPic.style.display = "none";
+    munafPic.style.display = "none";
+    nathPic.style.display = "none";
+    blur.style.display = "none";
+}
+
+function popupsehwag(){
+    sehwagPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupsachin(){
+    sachinPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupgautam(){
+    gautamPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupvirat(){
+    viratPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupdhoni(){
+    dhoniPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupyuvraj(){
+    yuvrajPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupraina(){
+    rainaPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popuphar(){
+    harPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupkhan(){
+    khanPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupmunaf(){
+    munafPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
+
+function popupnath(){
+    nathPic.style.display = "block";
+    close.style.display = "block";
+    blur.style.display = "block";
+}
